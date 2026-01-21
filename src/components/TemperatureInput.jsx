8cd23c45a3f7df5f7e4aeb1851b34ddb82b03225
Trinoy Kundu <trinoykundu@gmail.com>
2026-01-21 23:17:18 +0600

    update TemperatureInput

--- src/components/TemperatureInput.jsx
+++ src/components/TemperatureInput.jsx
@@ -19,23 +19,3 @@ export default function TemperatureInput({temperature, scale, onTemperatureChang
         </fieldset>
     )
 }
-
-// import React from 'react';
-
-// const scaleNames = {
-//     c: 'Celsius',
-//     f: 'Fahrenheit',
-// };
-
-// export default function TemperatureInput({ temperature, scale, onTemperatureChange }) {
-//     return (
-//         <fieldset>
-//             <legend>Enter temperature in {scaleNames[scale]}:</legend>
-//             <input
-//                 type="text"
-//                 value={temperature}
-//                 onChange={(e) => onTemperatureChange(e, scale)}
-//             />
-//         </fieldset>
-//     );
-// }
