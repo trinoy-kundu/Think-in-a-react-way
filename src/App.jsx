e2a2b69728afa5095d6c28b43f9afa107bf28733
Trinoy Kundu <trinoykundu@gmail.com>
2026-01-22 00:26:58 +0600

    Complete styling for App and TemperatureInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import Calculator from "./components/calculator";
 
 export default function App() {
--- src/components/TemperatureInput.jsx
+++ src/components/TemperatureInput.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./TemperatureInput.css"; // Add this line
 
 const scaleNames = {
     c: 'Celsius',
